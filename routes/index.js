e83c8cdd056382d0035d25841dace49268c79b5c
1712722@student.hcmus.edu.vn <1712722@student.hcmus.edu.vn>
2020-11-03 14:26:59 +0700

    s

--- routes/index.js
+++ routes/index.js
@@ -9,24 +9,24 @@ router.get('/', function (req, res, next) {
 router.post('/', (req, res) => {
   const number = req.body.number;
   // const result = Math.pow(parseFloat(number), 2);
-  const result = number + number;
+  const result = parseFloat(number) + parseFloat(number);
   const firstNumber = req.body.firstNumber;
   const secondNumber = req.body.secondNumber;
   const gender = req.body.gender;
   const mainResult = req.body.mainResult;
-  if (gender == 100) {
+  if (parseFloat(gender) == 100) {
     mainResult = Math.pow(parseFloat(firstNumber), 2);;
   }
-  if (gender == 200) {
-    mainResult = firstNumber - secondNumber;
+  if (parseFloat(gender) == 200) {
+    mainResult = parseFloat(firstNumber) - parseFloat(secondNumber);
 
   }
-  if (gender == 300) {
-    mainResult = firstNumber * secondNumber;
+  if (parseFloat(gender) == 300) {
+    mainResult = parseFloat(firstNumber) * parseFloat(secondNumber);
 
   }
-  if (gender == 400) {
-    mainResult = firstNumber / secondNumber;
+  if (parseFloat(gender) == 400) {
+    mainResult = parseFloat(firstNumber) / parseFloat(secondNumber);
 
   }
   res.render('index', { title: 'Tinh xong', firstNumber, secondNumber, mainResult, number, result });
